2794b68b863e042ea3afe7dfd70574e1e0276592
Aurel Zefi <aurelzefi1994@gmail.com>
2021-03-09 16:05:10 +0100

    schedule job correctly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->job(FetchExternalPosts::class)->everyTwoHours();
+        $schedule->job(new FetchExternalPosts())->everyTwoHours();
     }
 
     /**
