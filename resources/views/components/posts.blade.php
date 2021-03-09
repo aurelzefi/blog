70876890f9a2d0a900d9771e13ad48a9e262bdc7
Aurel Zefi <aurelzefi1994@gmail.com>
2021-03-09 17:15:37 +0100

    wip

--- resources/views/components/posts.blade.php
+++ resources/views/components/posts.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="flex justify-end">
-        <form method="GET" class="mb-4">
+        <form method="GET" class="mb-4 mr-4 md:mr-0">
             <x-select name="sort" :options="['desc' => __('Latest To First'), 'asc' => __('First To Latest')]" :current="$sort" />
 
             <input type="hidden" name="page" value="{{ $posts->currentPage() }}" />
@@ -11,31 +11,39 @@
         </form>
     </div>
 
-    @foreach ($posts as $post)
-        <div class="mb-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 bg-white border-b border-gray-200">
-                <div class="flex justify-between items-center">
-                    <div>
-                        <span>
-                            {{ $post->title }}
-                        </span>
+    @if ($posts->count())
+        @foreach ($posts as $post)
+            <div class="mb-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="flex justify-between items-center">
+                        <div>
+                            <span>
+                                {{ $post->title }}
+                            </span>
 
-                        <span class="block mt-1 text-sm text-gray-700">
-                            {{ __('Posted by :user', ['user' => $post->user->name]) }}
+                            <span class="block mt-1 text-sm text-gray-700">
+                                {{ __('Posted by :user', ['user' => $post->user->name]) }}
+                            </span>
+                        </div>
+
+                        <span class="text-sm text-gray-700 whitespace-nowrap">
+                            {{ $post->published_at->diffForHumans() }}
                         </span>
                     </div>
 
-                    <span class="text-sm text-gray-700 whitespace-nowrap">
-                        {{ $post->published_at->diffForHumans() }}
-                    </span>
+                    <p class="mt-4">
+                        {{ $post->description }}
+                    </p>
                 </div>
-
-                <p class="mt-4">
-                    {{ $post->description }}
-                </p>
+            </div>
+        @endforeach
+    @else
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 bg-white border-b border-gray-200">
+                {{ __('No posts to show.') }}
             </div>
         </div>
-    @endforeach
+    @endif
 
     {{ $posts->links() }}
 </div>
